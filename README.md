c5e160e164c5be36014bc13318ba80d4a039a7ce
Kat Tipton <kathryn.tipton@gmail.com>
2023-06-20 21:54:50 -0400

    Add csv format param, update readme

--- README.md
+++ README.md
@@ -19,25 +19,39 @@ Given a target postage, some stamp denominations, and how many stamps you wanna 
 
 ### Examples
 
-Using six stamps, make the US Letter postage:  
+Using two stamps and limiting overpayment by 20, make the US Letter postage and output as csv:  
 ```bash
-./bin/calculate_postage --number 6 --postage 63
+./bin/calculate_postage --number 2 --postage 63 --overpay 20 --csv
+target,num_stamps,overpayment,stamps
+63,1,0,[63]
+63,2,9,"[48, 24]"
+63,2,17,"[40, 40]"
+63,2,1,"[40, 24]"
 ```
 
-Using four stamps, make the US Postcard postage:  
+Using three stamps and limiting overpayment to 50, make the US International postage:  
 ```bash
-./bin/calculate_postage --number 4
-```
+❯ ./bin/calculate_postage --overpay 50 -p 145 -n3
+There are 39 ways to make 145 in 3 stamps without overpaying by more than 50.
 
-Using seven stamps, make the US International postage:  
-```bash
-./bin/calculate_postage --number 6 --postage 145
-```
-
-Using three stamps and a custom set of stamp values and postage:  
-(currently bugged as a command line argument, must be modified in-code)  
-```bash
-./bin/calculate_postage --number 6 --postage 55 --stamps 1,2,3,5,8,13,21,34,55,89
+145:   1/39 use this denom
+111:   5/39 use this denom
+103:  11/39 use this denom
+100:   8/39 use this denom
+ 87:  10/39 use this denom
+ 63:  10/39 use this denom
+ 48:  10/39 use this denom
+ 40:  17/39 use this denom
+ 24:   9/39 use this denom
+ 10:   4/39 use this denom
+  5:   2/39 use this denom
+  4:   1/39 use this denom
+  3:   1/39 use this denom
+  2:   1/39 use this denom
+  1:   0/39 use this denom
+with  1 stamps: [[145]]
+with  2 stamps: [[111, 63], [111, 48], [111, 40], [103, 87], [103, 63], [103, 48], [100, 87], [100, 63], [100, 48], [87, 87], [87, 63]]
+with  3 stamps: [[111, 24, 24], [111, 24, 10], [103, 40, 40], [103, 40, 24], [103, 40, 10], [103, 40, 5], [103, 40, 4], [103, 40, 3], [103, 40, 2], [103, 24, 24], [100, 40, 40], [100, 40, 24], [100, 40, 10], [100, 40, 5], [100, 24, 24], [87, 48, 48], [87, 48, 40], [87, 48, 24], [87, 48, 10], [87, 40, 40], [87, 40, 24], [63, 63, 63], [63, 63, 48], [63, 63, 40], [63, 63, 24], [63, 48, 48], [63, 48, 40]]
 ```
 
 ## Testing
--- lib/valid_postage.rb
+++ lib/valid_postage.rb
@@ -13,7 +13,8 @@ class ValidPostage
     postcard: 48
   }.freeze
 
-  attr_accessor :stamp_limit, :overpay_limit, :target_postage, :valid_postage, :stamp_denoms, :calculations_complete, :valid_denoms, :verbose
+  attr_accessor :stamp_limit, :overpay_limit, :target_postage, :valid_postage, :stamp_denoms, :calculations_complete,
+                :valid_denoms, :verbose, :csv
 
   # By default, what can we do with 3 US stamps on a postcard, limit overspending to 10 cents
   def initialize
@@ -24,6 +25,7 @@ class ValidPostage
     self.stamp_denoms = US_POSTAGE_STAMPS.sort.reverse
 
     self.verbose = false
+    self.csv = false
 
     # Working vars
     self.valid_postage = []
@@ -33,7 +35,7 @@ class ValidPostage
 
   # don't make the external interface think about the args
   def calculate
-    puts "Let's do this!" if self.verbose
+    puts "Let's do this!" if verbose
     calculate_combinations(1, [], stamp_denoms)
     # in case we didn't exhaust depth during calculations
     self.calculations_complete = true
@@ -41,16 +43,16 @@ class ValidPostage
 
   # actual calculation logic
   def calculate_combinations(current_depth, applied_postage, working_denoms)
-    prefix ="  "*current_depth
-    puts "#{prefix}Applied #{applied_postage}. Checking #{working_denoms.first}" if self.verbose
+    prefix = '  ' * current_depth
+    puts "#{prefix}Applied #{applied_postage}. Checking #{working_denoms.first}" if verbose
 
     if calculations_complete
-      puts "#{prefix}| Nothing more can make postage on this chain" if self.verbose
+      puts "#{prefix}| Nothing more can make postage on this chain" if verbose
       return
     end
 
     if working_denoms.empty?
-      puts "#{prefix}| Denoms all checked on this chain" if self.verbose
+      puts "#{prefix}| Denoms all checked on this chain" if verbose
       return
     end
 
@@ -59,12 +61,12 @@ class ValidPostage
     # We are over our stamp count, return
     if current_depth > stamp_limit
       # we got to the depth limit with a single denom and it wasn't valid, quit checking
-      print "#{prefix}| We are out of stamp slots" if self.verbose
+      print "#{prefix}| We are out of stamp slots" if verbose
       if applied_postage.first == active_denom && !valid_denoms[active_denom]
-        print "#{prefix}| and shouldn't check any further down this chain" if self.verbose
+        print "#{prefix}| and shouldn't check any further down this chain" if verbose
         self.calculations_complete = true
       end
-      puts if self.verbose
+      puts if verbose
       return
     end
 
@@ -72,13 +74,13 @@ class ValidPostage
     new_postage = [*applied_postage, active_denom]
 
     if new_postage.sum >= target_postage # We made postage,
-      puts "#{prefix}| We can make postage this level" if self.verbose
+      puts "#{prefix}| We can make postage this level" if verbose
       valid_postage.push(new_postage)
       valid_denoms[active_denom] = true
     else
       # Otherwise, continue depth-wise
       depth_copy = working_denoms.map(&:clone)
-      puts "#{prefix}> Let's add another stamp!" if self.verbose
+      puts "#{prefix}> Let's add another stamp!" if verbose
       calculate_combinations(current_depth + 1, new_postage, depth_copy)
     end
 
@@ -87,13 +89,17 @@ class ValidPostage
     # call at our same depth with the same applied postage passed into us
     breadth_copy = working_denoms.map(&:clone)
     breadth_copy.shift
-    puts "#{prefix}> Let's check the next denomination!" if self.verbose
+    puts "#{prefix}> Let's check the next denomination!" if verbose
     calculate_combinations(current_depth, applied_postage, breadth_copy)
   end
 
   # eventually, support other outputs
   def output
-    format_summary
+    if csv
+      format_csv
+    else
+      format_summary
+    end
   end
 
   def format_csv
@@ -101,20 +107,15 @@ class ValidPostage
       csv << %w[target num_stamps overpayment stamps]
       valid_postage.each do |postage|
         overpay = postage.sum - target_postage
-        next if overpay > self.overpay_limit
+        next if overpay > overpay_limit
+
         csv << [target_postage, postage.size, overpay, postage.to_s]
       end
     end
   end
 
-  def format_pretty
-    (1..stamp_limit).each do |count|
-      count_stamps = valid_postage.select { |e| e.size == count }
-    end
-  end
-
   def format_summary
-    within_overpay = valid_postage.select { |e| e.sum - target_postage <= self.overpay_limit }
+    within_overpay = valid_postage.select { |e| e.sum - target_postage <= overpay_limit }
     counts = {}
     stamp_denoms.each do |denom|
       counts[denom] = within_overpay.select do |e|
@@ -124,7 +125,7 @@ class ValidPostage
 
     output = "There are #{within_overpay.size} ways to make #{target_postage} in #{stamp_limit} stamps without overpaying by more than #{overpay_limit}.\n\n"
     counts.each do |denom, count|
-      output += "%3d: " % denom
+      output += '%3d: ' % denom
       output += "%3d/#{within_overpay.size} use this denom\n" % count
     end
 
@@ -159,6 +160,10 @@ class ValidPostage
 
       parser.on('-o', '--overpay OVERPAY', Integer, 'How much you want to limit overpaying. Default 9999') do |overpay|
         self.overpay_limit = overpay
+      end
+
+      parser.on('-c', '--csv', 'Output csv format') do |csv|
+        self.csv = csv
       end
 
       parser.on('-s', '--stamps STAMPS', Array,
